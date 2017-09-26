2acd9fa0ffddc8ff6a40da008bfd6b6bfc8f12ae
Edgardo Garofalo <egarofalo83@gmail.com>
2017-09-26 13:27:06 -0300

    # Added - Se agregó una extensión MY_Controller de la clase CI_Controller del core de CodeIgniter. - Se actualizó el manual.

---
+++ MY_Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class MY_Controller extends CI_Controller {
+    
+    protected $em;
+    
+    public function __construct() {
+        parent::__construct();
+        $this->em = $this->doctrigniter->getEntityManager();
+    }
+    
+}
