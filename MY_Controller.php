def9b2ed2c58f1c10d0f74bc4f149cac5a7c430c
Edgardo Garofalo <egarofalo83@gmail.com>
2017-09-26 16:17:17 -0300

    Update MY_Controller.php

--- MY_Controller.php
+++ MY_Controller.php
@@ -2,14 +2,13 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 class MY_Controller extends CI_Controller {
     
     protected $em;
     
     public function __construct() {
         parent::__construct();
-        $this->em = $this->doctrigniter->getEntityManager();
+        $this->em = $this->doctrine->getEntityManager();
     }
     
 }
